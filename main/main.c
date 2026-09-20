29b4da151b7fb138a2af30c34c5862c105adbdc7
Eros Nucifora <erosnucifora2@gmail.com>
2026-09-20 19:28:58 -0300

    hola

--- main/main.c
+++ main/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
+
+#include "rs485_uart.h"
+#include "modbus_master.h"
 
 
 // La izquierda avanza y debe gobernar
 void app_main(void)
 {
+    
+
 
 }
